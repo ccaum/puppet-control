5d6696714003cfad076fe92c337f2b63850610b3
Carl Caum <carl@carlcaum.com>
2016-09-24 10:41:07 -0700

    Switch from SIMP modules to hardening.io modules

--- site/profile/manifests/compliance.pp
+++ site/profile/manifests/compliance.pp
@@ -2,34 +2,5 @@ class profile::compliance (
   Array $purge_unamanaged_resource_types = []
 ) {
 
-  # This module contains di cult to  nd RHEL packages such as sudosh2 # as well as SIMP speci c packages. We don’t need to include the
-  # SIMP speci c packages since we’re leveraging an existing Puppet
-  # Enterprise deployment
-  yumrepo { 'simp-packages':
-    ensure   => present,
-    baseurl  => "http://dl.bintray.com/simp/5.1.X", 
-    gpgcheck => 0,
-    exclude  => [
-      'mcollective.*',
-      'puppet.*',
-      'hiera.*',
-      'puppet.*',
-      'pupmod.*',
-      'razor.*',
-      'simp'
-    ],
-    before => [ Class['clamav','sudosh'] ],
-  }
-
-  class { 'aide': }
-  class { 'auditd': }
-
-  # Virus scanning. 
-  class { 'clamav': }
-
-  #Class[‘simplib’] expects this group to exist 
-  group { 'puppet':
-    ensure => present,
-    before => Class['simplib']
-  }
+  class { 'os_hardening': }
 }
